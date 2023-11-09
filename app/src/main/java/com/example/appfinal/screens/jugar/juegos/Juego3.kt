b5ce88df8ca837fa753dcca9c8ef047b9f5713fb
Isabella Garduño <105307704+a00833573@users.noreply.github.com>
2023-11-08 23:03:51 -0600

    Correcciones del merge

--- app/src/main/java/com/example/appfinal/screens/jugar/juegos/Juego3.kt
+++ app/src/main/java/com/example/appfinal/screens/jugar/juegos/Juego3.kt
@@ -1,17 +1,12 @@
 package com.example.appfinal.screens.jugar.juegos
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -26,14 +21,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.appfinal.screens.jugar.juegos.juego4.DraggableImage3
-import com.example.appfinal.screens.jugar.juegos.juego4.addNewImages3
 import com.example.appfinal.screens.jugar.juegos.juego4.generateImages3
-import kotlin.random.Random
 
 @Composable
 fun Juego3(navController: NavHostController) {
@@ -45,7 +36,6 @@ fun Juego3(navController: NavHostController) {
     var currentNumber by remember { mutableStateOf(1) }
 
     Box(
-
         modifier = Modifier
             .fillMaxSize()
             .background(color = azulClaro)
@@ -60,7 +50,6 @@ fun Juego3(navController: NavHostController) {
                 }
             },
             modifier = Modifier
-                .align(Alignment.TopEnd)
                 .padding(8.dp),
             contentPadding = PaddingValues(8.dp)
         ) {
@@ -75,5 +64,14 @@ fun Juego3(navController: NavHostController) {
                 color = Color.White,
                 fontWeight = FontWeight.Bold)
         }
+
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Juego 3")
+        }
+
     }
 }
